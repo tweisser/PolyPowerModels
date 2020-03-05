a023aa772d94196653f8fbde137234379ae734f3
tweisser <tillmann.weisser@web.de>
2020-03-05 05:25:26 -0700

    test model

--- test/model.jl
+++ test/model.jl
@@ -1,18 +1,34 @@
 @testset "PolyModel" begin
+	@test sense(nothing) == nothing
     m = PolyModel()
     @test sprint(show, m) ==
     """
     Feasibility problem
     s.t.
     """
+	@test objective_function(m) == nothing
     @polyvar x y
-    set_objective!(m, MIN, x^4*y^2 + x^2*y^4 - 3*x^2*y^2 + 1)
+    set_objective!(m, MAX, x^4*y^2 + x^2*y^4 - 3*x^2*y^2 + 1)
     @test objective_function(m) == x^4*y^2 + x^2*y^4 - 3*x^2*y^2 + 1
-    @test objective_sense(m) == MIN
+    @test objective_sense(m) == MAX
     add_constraint!.(m, "xbox", [x, -x], GT, [0, 1])
     add_constraint!(m, "yset", y, EQ)
     add_constraint!(m, 1000*y, LT, x; normalize = true)
-    @test sprint(show, m) == "Minimize x^4*y^2 + x^2*y^4 - 3*x^2*y^2 + 1\ns.t.\nxbox : x ≥ 0\nxbox : -x - 1 ≥ 0\nyset : y = 0\n : -0.001*x + y ≤ 0\n"
+    @test sprint(show, m) == "Maximize x^4*y^2 + x^2*y^4 - 3*x^2*y^2 + 1\ns.t.\nxbox : x ≥ 0\nxbox : -x - 1 ≥ 0\nyset : y = 0\n : -0.001*x + y ≤ 0\n"
+	add_constraint!(m, x, EQ, y)
+    add_constraint!(m, "normalized", 1000*y, LT, x; normalize = true)
+    add_constraint!(m, PolyCon(1000*y, LT, x); normalize = true)
+	p = x + y
+	@test PolyPowerModels.remove_almost_zeros(1e-12*p) == 0
+	@test constraint_function(PolyCon( x, LT, y)) == x-y
+	@test sprint(show,PolyCon.(EQ, [x,y])) == "x = 0\ny = 0\n"
+	@test sense.(PolyPowerModels.split_equality(PolyCon(EQ, p))) == [LT, GT]
+	con = PolyCon(GT, p)
+	@test PolyPowerModels.split_equality(con) isa Vector{PolyCon}
+	@test sense(PolyPowerModels.invert_inequality(PolyCon(EQ, p))) == EQ
+	@test sense(PolyPowerModels.invert_inequality(PolyCon(LT, p))) == GT
+	@test sense(PolyPowerModels.invert_inequality(PolyCon(GT, p))) == LT
+	@test constraint_function(PolyPowerModels.invert_inequality(PolyCon(GT, p))) == p
  end
 
 @testset "PolyPowerModel" begin 
